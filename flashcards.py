c9eae26eebe823c9ca1495fc657b73427f205f7b
Alexandra Forsyth <alfo3794@user.uu.se>
2024-10-21 14:51:21 +0200

    Splash, Main, Lists, Practice

---
+++ flashcards.py
@@ -0,0 +1,66 @@
+math_flashdeck = [
+    "Matte 3",
+    {"question":"5+2?", "answer":"7", "hint":"6+1"},
+    ]
+
+it_flashdeck = [
+    "IT 3",
+    {"question":"Hur många bit är en byte?", "answer":"8", "hint":"1000"},
+    ]
+
+flashdecks = [
+    math_flashdeck,
+    it_flashdeck,
+    ]
+
+def splash():
+    print("Welcome to Flashdecks, a text-based flashcard program!")
+
+def main():
+    splash()
+    print("1) Practice" + "\n" + "2) My Flashdecks" +"\n" + "3) New Flashdeck" + "\n" + "4) Exit")
+    chosen_option = input("Option: ")
+    if chosen_option == "1":
+        choose_practice_deck()
+#         
+#     elif chosen_option == "2":
+#         
+#     elif chosen_option == "3":
+#         
+#     elif chosen_option == "4":
+#         shutdown()
+#     else:
+#         while not chosen_option == "1" and not chosen_option == "2" and not chosen_option == "3" and not chosen_option == "4":
+#             print("Please enter valid option.")
+#             chosen_option = input("Option: ")
+#         if chosen_option == "1":
+#             
+#         elif chosen_option == "2":
+#             
+#         elif chosen_option == "3":
+#             
+#         elif chosen_option == "4":
+#             shutdown()  
+
+def choose_practice_deck():
+    deck_number = 0
+    print("These are your flashdecks:")
+    for deck in flashdecks:
+        deck_number +=1
+        print(f"{deck_number}) {deck[0]}")
+    chosen_deck_input = int(input(f"Enter number of desired deck 1-{deck_number}: ")) -1
+    chosen_deck = flashdecks[chosen_deck_input]
+    practice(chosen_deck)
+    
+def practice(flashdeck):
+    for card in flashdeck:
+        if type(card) == dict: 
+            print(card["question"])
+# def flashdecks():
+#     print
+
+def shutdown():
+    print("Flashdecks shutting down.")
+    exit()
+    
+main()
