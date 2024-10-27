706b6f71d7e670032d2867b7ce74134188b418f5
Linnea Hesseltun <linnea.hesseltun@elev.ga.ntig.se>
2024-10-27 10:30:43 +0100

    göra flera frågor samtdigt, kan flyttas dock

--- flashcards.py
+++ flashcards.py
@@ -22,14 +22,14 @@ def splash():
 def main():
     splash()
     print("1) Practice" + "\n" + "2) My Flashdecks" +"\n" + "3) New Flashdeck" + "\n" + "4) Exit")
-    chosen_option = input("Option: ")
+    chosen_option = input("\nOption: ")
     if chosen_option == "1":
         choose_practice_deck()
 #         
 #     elif chosen_option == "2":
 #         
-#      elif chosen_option == "3":
-#          create_flashdeck(input("New flashdack title"))
+    elif chosen_option == "3":
+        create_flashdeck(input("\nNew flashdeck title  "))
 #         
 #     elif chosen_option == "4":
 #         shutdown()
@@ -69,15 +69,22 @@ def practice(flashdeck):
 #     print
 
 def create_flashdeck(title):
-    new_flashdeck_file = open(title, "a")
+    new_flashdeck_file = open(title + ".txt", "a")
     create_flashcard(new_flashdeck_file)
     
 def create_flashcard(file):
-    question = "\"" + input("Enter question") + "\""
-    answer = "\"" + input("Enter answer") + "\""
+    question = "\"" + input("Enter question   ") + "\""
+    answer = "\"" + input("Enter answer   ") + "\""
     
     file.write('{"question":'+ question + ', "answer":'+ answer + '},')
+
+    add_another = input("Do you want to add another one? (y/n) ")
+    if add_another == "y":
+        create_flashcard(file)
+    else:
+        main()
     
+
     
 def shutdown():
     print("Flashdecks shutting down.")
